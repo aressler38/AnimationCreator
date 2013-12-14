0983044baba91798aa6f5f0965a6729f559ec211
alexander <alexander@CygnusXG-1.(none)>
2013-12-14 02:03:19 -0800

    new files

---
+++ js/view/modal.view.js
@@ -0,0 +1,20 @@
+define(
+    [
+        "jQuery",
+        "underscore",
+        "Backbone", 
+        "renderTemplate",
+        "hbs!templates/modal"
+    ],
+    function($, _, Backbone, renderTemplate, modalTemplate) {
+        var ModalView = Backbone.View.extend({
+            
+            initialize: function() {
+                
+            }
+
+        });
+        return ModalView;
+    }
+);
+
